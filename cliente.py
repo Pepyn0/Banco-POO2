f72fe99a35ed5de18d5c5f3d192bb946c3813496
Pepyn0 <brpepy@gmail.com>
2021-04-26 10:41:59 -0300

    Adição de getters e setters na classe Pessoa

--- cliente.py
+++ cliente.py
@@ -1,6 +1,21 @@
 class Cliente(object):
 	def __init__(self, nome, sobrenome, cpf):
-		self.nome = nome
-		self.sobrenome = sobrenome
-		self.cpf = cpf
+		self._nome = nome
+		self._sobrenome = sobrenome
+		self._cpf = cpf
 
+	@property
+	def nome(self):
+		return self._nome
+
+	@nome.setter
+	def nome(self, nome):
+		self._nome = nome
+
+	@property
+	def sobrenome(self):
+		return self._sobrenome
+
+	@sobrenome.setter
+	def sobrenome(self, sobrenome):
+		self._sobrenome = sobrenome

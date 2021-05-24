f4e7c4b9caaa052cfe0453288109bd2c77b661b0
Vitoria Karolina Ferreira Sousa <vivikarolfe@gmail.com>
2021-05-24 08:17:07 -0300

    docstrings de comentários na conexaoCliente

--- FrontEnd/conexaoCliente.py
+++ FrontEnd/conexaoCliente.py
@@ -6,21 +6,37 @@ PORT = 8000
 
 class ConexaoClient(object):
 	def __init__(self):
+	"""
+		AUTHOR: Pablo Duarte da Silva e Vitoria Karolina Ferreira de Sousa
+
+	"""
 		self.addr = None
 		self.clientSocket = None
 
 
 	def conectar(self):
+		"""
+		DESCRIPTION: Função responsável pela conexão do cliente ao servidor
+
+		"""
 		self.addr = ((HOST, PORT))
 		self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.clientSocket.connect(self.addr)
 
 
 	def comunicar(self, dadosEnviados: str):
+		"""
+		DESCRIPTION: Nesta função ocorre o envio dos dados informados pelo cliente na rede.
+
+		"""
 		self.clientSocket.send(dadosEnviados.encode())
 		dadosRecebidos = self.clientSocket.recv(1024).decode()
 		return dadosRecebidos
 
 
 	def fechar(self):
+		"""
+		DESCRIPTION: Função de encerramento de conexão e envio de dados.
+
+		"""
 		self.clientSocket.close()
